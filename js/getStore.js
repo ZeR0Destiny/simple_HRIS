54fb51294901cf606ae46e98795b4ba4441dcfa9
ZeR0Destiny <67914680+ZeR0Destiny@users.noreply.github.com>
2023-08-10 01:21:43 -0400

    added all store list

--- js/getStore.js
+++ js/getStore.js
@@ -11,27 +11,48 @@ function loadStores() {
     switch (selectedRegion) {
         case 'CENTRAL':
             stores = [
-
+                'NYKD', 'FV', 'B223', '#6', 'AG', 'PW', '#18', 'SWD', '#39', '#72', 'RMP', '#6464Y', 'HCM', 'AMP', 'UCM', 'MKV', 'UV'
             ];
             break;
         case 'EAST':
+            stores = [
+                'STC', 'CW', 'PTC', 'OC', 'STM', 'YSC', 'EPW', 'KAW', 'CQC'   
+            ];
             break;
         case 'NW':
+            stores = [
+                'BCC', 'SPW', 'WB', 'PPB', 'AL', 'TPO', 'RCB', 'VM', 'GGM'
+            ];
             break;
         case 'OTTAWA':
+            stores = [
+                'BSC', 'SLT', 'RDC', 'PDO', 'BLB', 'TOO', 'RCO', 'BAF', 'SSC'
+            ];
             break;
         case 'QUEBEC':
             stores = [
-                'FPC', 'SCS', 'GDA', 'CFL', 'MCP', 'MEC', 'SCO', 'WTC', 'CAN', 'PSB', 'BSN'
+                'FPC', 'SCS', 'GDA', 'CFL', 'MCP', 'MEC', 'SCO', 'WTC', 'CAN', 'PSB', 'BSN', 'CDD'
             ];
             break;
         case 'SOUTH':
+            stores = [
+                'TEC', '223Q', 'CLP', '#660', '#167', 'K9', 'LS', 'ES', '#108', 'DFM', '#60', 'PMH'
+            ];
             break;
         case 'SW1':
+            stores = [
+                'RW', 'SQ1', 'SC', 'DX', 'EM', 'CVP', 'OV', 'BLT', 'BPV'
+            ];
             break;
         case 'SW2':
+            stores = [
+                'LR', 'FVP', 'MVP', 'EGS', 'CBC', 'SRM', 'WOM', 'GKS', 'CTG', 'FSC', 'MVC', 'OCM', 'TPC', 'DVS'
+            ];
             break;
         case 'USA':
+            stores = [
+                'GSP', 'BWC', 'WBC', 'MLP', 'NPC', 'MSH', 'ADM', 'MJG', 'CHM', 'HSC', 'CTM', 'JSS', 'WLB', 'FRM', 'PMM', 'RWT', 'KOP', 'GAM', 'PPO'
+            ];
             break;
         default:
             break;
