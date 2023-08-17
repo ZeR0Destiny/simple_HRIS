f5639625a15bf55abe0c5436a1913b2fb9642245
ZeR0Destiny <67914680+ZeR0Destiny@users.noreply.github.com>
2023-08-16 21:57:53 -0400

    dashboard

--- index.php
+++ index.php
@@ -43,14 +43,14 @@ $database = new DB_Manager();
               </div>
 
               <div class="card-body">
-                <h5 class="card-title">Sales <span>| Today</span></h5>
+                <h5 class="card-title">Employee <span>| Total</span></h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-cart"></i>
+                    <i class="bi bi-people"></i>
                   </div>
                   <div class="ps-3">
-                    <h6>145</h6>
+                    <h6><?php $total = $database->employee_total(); echo $total['employee']; ?></h6>
                     <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
 
                   </div>
@@ -78,7 +78,7 @@ $database = new DB_Manager();
               </div>
 
               <div class="card-body">
-                <h5 class="card-title">Revenue <span>| This Month</span></h5>
+                <h5 class="card-title">Employee <span>| Active</span></h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
@@ -114,7 +114,7 @@ $database = new DB_Manager();
               </div>
 
               <div class="card-body">
-                <h5 class="card-title">Employee <span id="filterLabel">| Today</span></h5>
+                <h5 class="card-title">Employee <span id="filterLabel">| Inactive</span></h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
