aacaec10743ab61861fd2068caa54fbbfe60676f
ZeR0Destiny <67914680+ZeR0Destiny@users.noreply.github.com>
2023-09-16 23:21:23 -0400

    postal code does not require dash

--- js/getPostalCode.js
+++ js/getPostalCode.js
@@ -1,5 +1,5 @@
 function checkPostalCode() {
-  const cad = /[A-Z][0-9][A-Z]-[0-9][A-Z][0-9]/g;
+  const cad = /[A-Z][0-9][A-Z][0-9][A-Z][0-9]/g;
   const us = /[0-9]{5}/g;
 
   let code = document.getElementById("inputCountry").value;
