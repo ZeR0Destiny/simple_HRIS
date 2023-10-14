b34959d755bc55116d68c825cccf43db4f73671e
ZeR0Destiny <67914680+ZeR0Destiny@users.noreply.github.com>
2023-10-14 01:10:20 -0400

    Employee info view table control

--- js/table-control.js
+++ js/table-control.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-  $("#emp_table, #emp_file_table, #file_table").DataTable({
+  var id_emp;
+
+  $("#emp_table, #emp_file_table, #user_table").DataTable({
     responsive: {
       details: {
         type: "column",
@@ -10,40 +12,41 @@ $(document).ready(function () {
 
   var table = $("#emp_table").DataTable();
   var table2 = $("#emp_file_table").DataTable();
-  var table3 = $("#file_table").DataTable();
+  var table3 = $("#user_table").DataTable();
 
-  var table4 = $("#file_table_benefit").DataTable();
-  var table5 = $("#file_table_medical").DataTable();
-  var table6 = $("#file_table_discipline").DataTable();
-  var table7 = $("#file_table_hiring").DataTable();
-  var table8 = $("#file_table_immigration").DataTable();
-  var table9 = $("#file_table_investigation").DataTable();
-  var table8 = $("#file_table_personal").DataTable();
-  var table8 = $("#file_table_training").DataTable();
-  var table9 = $("#file_table_unclassified").DataTable();
-  var table8 = $("#file_table_work").DataTable();
+  $("button").unbind("click");
 
   $("#emp_table tbody").on("click mouseover", "tr", function () {
     var data = table.row(this).data();
 
-    $("button").unbind("click");
     $("button").click(function () {
       if (Array.isArray(data)) {
         id_emp = data[0];
       }
       console.log(id_emp);
 
+      $("button").unbind("click");
+
       $.ajax({
         url: "emp_info.php",
         type: "post",
         data: {
-          emp_id: id_emp,
+            emp_id: id_emp,
         },
         success: function (get_employee) {
-          $(".modal-body").html(get_employee);
-          $("#exampleModal").modal("show");
+            $(".modal-body").html(get_employee);
+            $("#exampleModal").modal("show");
         },
-      });
+        error: function (xhr, status, error) {
+            console.error(error);
+            // Handle the error, e.g., display an error message
+        },
+    });
+    
+
     });
   });
+
+
 });
+
