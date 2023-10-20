2663f1c73faf71041d0aa4a8083dc94a15b34c8c
ZeR0Destiny <67914680+ZeR0Destiny@users.noreply.github.com>
2023-10-19 22:49:07 -0400

    Changed submit if condition; Added verify exixting condition

--- Controller/account-controller.php
+++ Controller/account-controller.php
@@ -10,7 +10,7 @@ session_start();
 
 $database = new Account_DB_Manager();
 
-// When the login is clicked will verify the result of the login function and redirect to the correct pages
+// When the login is clicked, it will verify the result of the login function and redirect to the correct pages
 if (isset($_POST["login"])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -32,17 +32,29 @@ function logout()
     exit;
 }
 
+// When the register is clicked, it will create a new user if the terms is checked
 if (isset($_POST["register"])) {
     if (isset($_POST['terms'])) {
-        $new_user = array("name" => ucwords($_POST["name"]), "username" => $_POST["username"], "password" => $_POST["password"]);
+        if ($database->verify_existing($_POST["username"], $_POST["password"]) == false) {
+            $new_user = array("name" => ucwords($_POST["name"]), "username" => $_POST["username"], "password" => $_POST["password"]);
 
-        $user = new user($new_user);
+            $user = new user($new_user);
 
-        $database->register($user);
+            $database->register($user);
+        } else {
+            header("location: pages-register.php?Failed_to_register");
+        }
     }
 }
 
 if (isset($_GET['user_ID'])) {
     $database->delete_user($_GET['user_ID']);
     $database->reset_auto_increment();
+}
+
+if (isset($_POST['submit'])) {
+    $userId = $_POST['userId'];
+    $password = $_POST['changePassword'];
+
+    $database->change_password($userId, $password);
 }
